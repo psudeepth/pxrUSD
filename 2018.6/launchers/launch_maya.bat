5e5cd309682d0534e3a18864c43751c39e4c5e82
Sudeepth Patinjarayil <psudeepth@gmail.com>
2021-09-07 20:39:18 +0530

    Build with QT

--- 2018.6/launchers/launch_maya.bat
+++ 2018.6/launchers/launch_maya.bat
@@ -2,7 +2,6 @@
 
 echo Opening Maya 2018.6 with Pixar USD Support...
 
-set PYTHONPATH=%PYTHONPATH%;C:\pxrUSD\pxrUSDCore\lib\python
 set PATH=%PATH%;C:\pxrUSD\pxrUSDCore\bin;C:\pxrUSD\pxrUSDCore\lib
 set MAYA_MODULE_PATH=%MAYA_MODULE_PATH%;C:\pxrUSD\pxrUSDMayaCore
 
--- 2020.4/launchers/launch_maya.bat
+++ 2020.4/launchers/launch_maya.bat
@@ -2,7 +2,6 @@
 
 echo Opening Maya 2020.4 with Pixar USD Support...
 
-set PYTHONPATH=%PYTHONPATH%;D:\pxrUSD\2020.4\pxrUSDCore\lib\python
 set PATH=%PATH%;D:\pxrUSD\2020.4\pxrUSDCore\bin;D:\pxrUSD\2020.4\pxrUSDCore\lib
 set MAYA_MODULE_PATH=%MAYA_MODULE_PATH%;D:\pxrUSD\2020.4\pxrUSDMayaCore
 
